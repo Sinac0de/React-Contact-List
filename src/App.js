6bc9f20f198ebd768c3f43975884e76c8d078033
Sina Moradian <sinacsharp@gmail.com>
2023-07-31 06:44:45 +0330

    Add ContactList, AddContact components. Also Add their handlers.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useId, useState } from "react";
 import "./App.css";
+import AddContact from "./components/AddContact/AddContact";
+import ContactList from "./components/ContactList/ContactList";
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+  const addContactHandler = (contact) => {
+    setContacts([...contacts, { ...contact, id: Date.now() }]);
+  };
+
+  const handleRemoveContact = (id) => {
+    const filteredContacts = contacts.filter((contact) => contact.id !== id);
+    setContacts(filteredContacts);
+  };
+
   return (
     <main className="App">
-      <h1>Contact App</h1>
-      <section>Add contact</section>
-      <section>Contact list</section>
+      <h1>Contact List App</h1>
+      <section>
+        <AddContact addContactHandler={addContactHandler} />
+      </section>
+      <section>
+        <ContactList contacts={contacts} removeContact={handleRemoveContact} />
+      </section>
     </main>
   );
 }
